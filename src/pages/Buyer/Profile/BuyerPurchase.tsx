834840d8448960241e8b81e4b282f5611ff353d8
dihuynh@outlook.sg <dihuynh@outlook.sg>
2024-07-08 17:29:48 +0700

    feat: add pagination orders

--- src/pages/Buyer/Profile/BuyerPurchase.tsx
+++ src/pages/Buyer/Profile/BuyerPurchase.tsx
@@ -1,49 +1,54 @@
-import { Buyer_QueryKeys } from '@/constants/query-keys'
 import useAxiosBuyerPrivate from '@/hooks/useAxiosBuyerPrivate'
 import { OrderItem } from '@/types/order.type'
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
-import { useState } from 'react'
+import { useMutation } from '@tanstack/react-query'
+import { useEffect, useState } from 'react'
 import OrderItemComponent from './components/OrderItem'
-import { LoadingComponent } from '@/components'
 
 const BuyerPurchase = () => {
   const [activeTab, setActiveTab] = useState('')
   const axiosPrivate = useAxiosBuyerPrivate()
   const [orderListByStatus, setOrderListByStatus] = useState<OrderItem[]>([])
-  const client = useQueryClient()
+  const [currentPage, setCurrentPage] = useState<number>(0)
+  const [pageSize, setPageSize] = useState<number>(6)
+  const [totalPages, setTotalPages] = useState<number>(0)
+  const [loading, setLoading] = useState<boolean>(false)
 
-  const { isLoading: isLoadingOrders } = useQuery({
-    queryKey: [Buyer_QueryKeys.ORDER_LIST],
-    queryFn: async () => {
-      const resp = await axiosPrivate.get('/buyer/order')
-      setOrderListByStatus(resp.data.data.content)
-      return resp.data.data.content
-    },
-    enabled: activeTab === '',
-  })
+  const fetchOrders = async (status = '', page = 0, size = 6) => {
+    setLoading(true)
+    const endpoint = status
+      ? `/buyer/order/by-status?status=${status}&currentPage=${page}&pageSize=${size}`
+      : `/buyer/order?currentPage=${page}&pageSize=${size}`
+    const resp = await axiosPrivate.get(endpoint)
+    setOrderListByStatus(resp.data.data.content || [])
+    setCurrentPage(resp.data.data.currentPage)
+    setTotalPages(resp.data.data.totalPages)
+    setPageSize(resp.data.data.pageSize)
+    setLoading(false)
+    return resp.data.data.content
+  }
 
-  const getOrderListByStatusMutation = useMutation({
-    mutationFn: async (status: string) => {
-      const resp = await axiosPrivate.get(`/buyer/order/by-status?status=${status}`)
-      setOrderListByStatus(resp.data.data.content)
-      return resp
-    },
-    onSuccess: (resp) => {
-      setOrderListByStatus(resp.data.data.content)
-    },
+  const fetchOrdersMutation = useMutation({
+    mutationFn: async () => fetchOrders(activeTab, currentPage, pageSize),
   })
 
   const handleTabClick = (tabName: string) => {
     setActiveTab(tabName)
-    if (tabName !== '') {
-      getOrderListByStatusMutation.mutate(tabName)
-    } else {
-      client.invalidateQueries({ queryKey: [Buyer_QueryKeys.ORDER_LIST, activeTab] })
-    }
+    setCurrentPage(0) // Reset page when tab changes
+    fetchOrdersMutation.mutate()
   }
 
-  if (isLoadingOrders) {
-    return <LoadingComponent />
+  useEffect(() => {
+    fetchOrdersMutation.mutate()
+  }, [currentPage, pageSize, activeTab])
+
+  const statusLabels: { [key: string]: string } = {
+    TO_SHIP: 'Chờ vận chuyển',
+    SHIPPING: 'Đang giao',
+    COMPLETED: 'Đã nhận',
+    CANCELLED: 'Đã hủy',
+    FAILED_DELIVERY: 'Thất bại',
+    REFUNDED: 'Hoàn tiền/Trả hàng',
+    '': 'Tất cả',
   }
 
   return (
@@ -59,93 +64,60 @@ const BuyerPurchase = () => {
       <div className="my-2">
         <div role="tablist" className="lg:tabs tabs-lifted">
           <div className="lg:flex">
-            <div>
-              <div
-                onClick={() => handleTabClick('TO_SHIP')}
-                role="tab"
-                className={`tab ${activeTab === 'TO_SHIP' ? 'tab-active font-semibold text-primary ' : 'indicator'}`}
-              >
-                Chờ vận chuyển
-                <span className={`${activeTab === 'TO_SHIP' ? 'badge text-primary' : 'indicator-item badge'}`}>
-                  {activeTab === 'TO_SHIP' && orderListByStatus.length}
-                </span>
+            {Object.keys(statusLabels).map((status) => (
+              <div key={status}>
+                <div
+                  onClick={() => handleTabClick(status)}
+                  role="tab"
+                  className={`tab ${activeTab === status ? 'tab-active font-semibold text-primary ' : 'indicator'}`}
+                >
+                  {statusLabels[status]}
+                  <span className={`${activeTab === status ? 'badge text-primary' : 'indicator-item badge'}`}>
+                    {activeTab === status && orderListByStatus.length}
+                  </span>
+                </div>
               </div>
-            </div>
-            <div>
-              <div
-                role="tab"
-                onClick={() => handleTabClick('SHIPPING')}
-                className={`tab ${activeTab === 'SHIPPING' ? 'tab-active font-semibold text-primary' : 'indicator'}`}
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-between items-center mb-6">
+        <p>Tất cả sản phẩm</p>
+        <div className="flex gap-2 items-center">
+          <div className="text-sm">
+            Trang {currentPage + 1}/{totalPages}
+          </div>
+          <div className="join">
+            {Array.from({ length: totalPages }, (_, i) => i).map((page) => (
+              <button
+                onClick={() => setCurrentPage(page)}
+                key={page}
+                className={`join-item btn-xs ${page === currentPage ? 'btn btn-active btn-primary text-white' : 'btn'}`}
               >
-                Đang giao
-                <span className={` ${activeTab === 'SHIPPING' ? 'badge text-primary' : 'indicator-item badge'}`}>
-                  {activeTab === 'SHIPPING' && orderListByStatus.length}
-                </span>
-              </div>
-              <div
-                role="tab"
-                onClick={() => handleTabClick('COMPLETED')}
-                className={`tab ${activeTab === 'COMPLETED' ? 'tab-active font-semibold text-primary' : 'indicator'}`}
-              >
-                Đã nhận
-                <span className={` ${activeTab === 'COMPLETED' ? 'badge text-primary' : 'indicator-item badge'}`}>
-                  {activeTab === 'COMPLETED' && orderListByStatus.length}
-                </span>
-              </div>
-
-              <div
-                role="tab"
-                onClick={() => handleTabClick('CANCELLED')}
-                className={`tab ${activeTab === 'CANCELLED' ? 'tab-active font-semibold text-primary' : 'indicator'}`}
-              >
-                Đã hủy
-                <span className={` ${activeTab === 'CANCELLED' ? 'badge text-primary' : 'indicator-item badge'}`}>
-                  {activeTab === 'CANCELLED' && orderListByStatus.length}
-                </span>
-              </div>
-              <div
-                role="tab"
-                onClick={() => handleTabClick('FAILED_DELIVERY')}
-                className={`tab ${
-                  activeTab === 'FAILED_DELIVERY' ? 'tab-active font-semibold text-primary' : 'indicator'
-                }`}
-              >
-                Thất bại
-                <span className={` ${activeTab === 'FAILED_DELIVERY' ? 'badge text-primary' : 'indicator-item badge'}`}>
-                  {activeTab === 'FAILED_DELIVERY' && orderListByStatus.length}
-                </span>
-              </div>
-              <div
-                role="tab"
-                onClick={() => handleTabClick('REFUNDED')}
-                className={`tab ${activeTab === 'REFUNDED' ? 'tab-active font-semibold text-primary' : 'indicator'}`}
-              >
-                Hoàn tiền/Trả hàng
-                <span className={` ${activeTab === 'REFUNDED' ? 'badge text-primary' : 'indicator-item badge'}`}>
-                  {activeTab === 'REFUNDED' && orderListByStatus.length}
-                </span>
-              </div>
-              <div
-                role="tab"
-                onClick={() => handleTabClick('')}
-                className={`tab ${activeTab === '' ? 'tab-active  font-semibold text-primary' : 'indicator'}`}
-              >
-                Tất cả
-                <span className={`${activeTab === '' ? 'badge text-primary' : 'badge indicator-item'}`}>
-                  {activeTab === '' && orderListByStatus.length}
-                </span>
-              </div>
-            </div>
+                {page + 1}
+              </button>
+            ))}
           </div>
         </div>
       </div>
       {/* Content */}
-      {orderListByStatus.map((order: OrderItem) => {
-        return <OrderItemComponent order={order} key={order.id} />
-      })}
+      {loading ? (
+        <div className="flex items-center justify-center">
+          <span className="loading loading-ring loading-xs"></span>
+          <span className="loading loading-ring loading-sm"></span>
+          <span className="loading loading-ring loading-md"></span>
+          <span className="loading loading-ring loading-lg"></span>
+        </div>
+      ) : (
+        <>
+          {orderListByStatus.map((order: OrderItem) => {
+            return <OrderItemComponent order={order} key={order.id} />
+          })}
 
-      {orderListByStatus.length === 0 && (
-        <div className="text-center my-2 italic text-gray-500">Bạn chưa có đơn hàng nào ở trạng thái này!</div>
+          {orderListByStatus.length === 0 && (
+            <div className="text-center my-2">Bạn chưa có đơn hàng nào ở trạng thái này!</div>
+          )}
+        </>
       )}
     </div>
   )
